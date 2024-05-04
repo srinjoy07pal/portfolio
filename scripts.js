5cfaea3a1d8d5492e78dae5ece36ee8f4894a0aa
Srinjoy Pal <144480997+srinjoy07pal@users.noreply.github.com>
2024-05-04 19:28:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
+const button = document.getElementById('myButton');
 
+button.addEventListener('click', function() {
+  console.log('Button was clicked!');
+  // Add your code here to handle the button click event
+});
